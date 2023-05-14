5b3a768da4435608b9d45c71a7f7cc5979cb4e4a
fathimawafa17 <131012727+fathimawafa17@users.noreply.github.com>
2023-05-14 21:55:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,84 @@
-const date_of_birth_2 = document.getElementById("dob");
-const date_of_today = new Date().toISOString().slice(4, 10);
+const form = document.getElementById('registration-form');
+const entriesTable = document.getElementById('entries-table');
+const storedEntries = JSON.parse(localStorage.getItem('entries')) || [];
 
-const year = new Date().getFullYear();
+// Define a function to add an entry to the table
+function addEntryToTable(name, email, password, dob, terms) {
+  // Create a new table row
+  const row = entriesTable.insertRow();
+  // Insert table cells for each form field value
+  const nameCell = row.insertCell();
+  const emailCell = row.insertCell();
+  const passwordCell = row.insertCell();
+  const dobCell = row.insertCell();
+  const termsCell = row.insertCell();
+  nameCell.innerHTML = name;
+  emailCell.innerHTML = email;
+  passwordCell.innerHTML = password;
+  dobCell.innerHTML = dob;
+  termsCell.innerHTML = terms ? "true" : "false";
+}
 
-date_of_birth_2.min = `${year - 55}${date_of_today}`;
-date_of_birth_2.max = `${year - 18}${date_of_today}`;
+// Define a function to validate email addresses
+function validateEmail(email) {
+  const emailRegex = /\S+@\S+\.\S+/;
+  return emailRegex.test(email);
+}
 
-let entries_of_users = [];
-let condition = true;
+// Define a function to calculate age based on date of birth
+function Calculateage(dob) {
+  const dobDate = new Date(dob);
+  const nowDate = new Date();
+  const age = nowDate.getFullYear() - dobDate.getFullYear();
+  const monthDiff = nowDate.getMonth() - dobDate.getMonth();
+  if (monthDiff < 0 || (monthDiff === 0 && nowDate.getDate() < dobDate.getDate())) {
+    return age - 1;
+  }
+  return age;
+}
 
-function submitListener(event) {
+// Load existing entries from localStorage
+storedEntries.forEach(entry => {
+  addEntryToTable(entry.name, entry.email, entry.password, entry.dob, entry.terms);
+});
+
+form.addEventListener('submit', function(event) {
   event.preventDefault();
-  document.getElementById("show_the_data").innerHTML = "";
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
-  let dob = document.getElementById("dob").value;
-  let acceptTerms = document.getElementById("tick_mark").checked;
+  const name = form.elements['name'].value;
+  const email = form.elements['email'].value;
+  const password = form.elements['password'].value;
+  const dob = form.elements['dob'].value;
+  const terms = form.elements['acceptedTerms'].checked;
 
-  let entries = {
-    name,
-    email,
-    password,
-    dob,
-    acceptTerms,
-  };
-  if (condition) {
-    let users = [];
-    users = JSON.parse(localStorage.getItem("user-entries")) || [];
-    for (let i = 0; i < users.length; i++) {
-      entries_of_users.push(users[i]);
-    }
-    condition = false;
+  // Validate email address
+  if (!validateEmail(email)) {
+    const formErrors = document.getElementById('form-errors');
+    formErrors.innerHTML = "Please enter a valid email address.";
+    formErrors.style.display = 'block';
+    return;
+  } else {
+    const formErrors = document.getElementById('form-errors');
+    formErrors.style.display = 'none';
   }
-  entries_of_users.push(entries);
-  localStorage.setItem("user-entries", JSON.stringify(entries_of_users));
-  show_the_data();
-}
-function show_the_data() {
-  let users = JSON.parse(localStorage.getItem("user-entries")) || [];
-  for (let i = 0; i < users.length; i++) {
-    let res = `<tr>
-    <td class="py-3 px-2">${users[i].name}</td>
-    <td class="py-3 px-2">${users[i].email}</td>
-    <td class="py-3 px-2">${users[i].password}</td>
-    <td class="py-3 px-2">${users[i].dob}</td>
-    <td class="py-3 px-2">${users[i].acceptTerms}</td>
-  </tr>`;
-    document.getElementById("show_the_data").innerHTML += res;
+
+  // Calculate age
+  const age = Calculateage(dob);
+
+  // Check age limit
+  if (age < 18 || age > 55) {
+    const formErrors = document.getElementById('form-errors');
+    formErrors.innerHTML = "You must be between 18 and 55 years old to register.";
+    formErrors.style.display = 'block';
+    return;
+  } else {
+    const formErrors = document.getElementById('form-errors');
+    formErrors.style.display = 'none';
   }
-}
-show_the_data();
+
+  // Create a new entry object and add to table and localStorage
+  const entry = { name, email, password, dob, terms };
+  storedEntries.push(entry);
+  localStorage.setItem('entries', JSON.stringify(storedEntries));
+  addEntryToTable(name, email, password, dob, terms);
+  form.reset();
+});
